b15bc61d3fcc0f2288b93fcd8bedea6c73d88a8c
hyt <hytmachineworks@gmail.com>
2017-08-07 00:11:51 +0900

    solve problem 05 to 06.

---
+++ codes/capter_01/problem_no_05.py
@@ -0,0 +1,37 @@
+# coding = utf-8
+"""
+create on : 2017/07/30
+project name : NLP_100
+file name : problem_no_05 
+
+problem : 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，
+          "I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
+"""
+
+
+def generate_n_gram(target_sequence, n):
+    """get n-gram from sequence
+    :param target_sequence: given sequence (string or list)
+    :param n: determine n-gram's n (2 = bi-gram, 3 = tri-gram, etc)
+    :return: calculate n-gram by list
+    """
+    sequence_length = len(target_sequence)
+    n_gram = [target_sequence[index_no: index_no + n] for index_no in range(sequence_length)]
+
+    return n_gram
+
+
+def problem_no_05(target_sequence):
+    """calculate bi-gram from given sequence
+    :param target_sequence: given sequence
+    :return: calculate result
+    """
+    string_bi_gram = generate_n_gram(target_sequence, 2)
+
+    word_list = target_sequence.split(" ")
+    word_bi_gram = generate_n_gram(word_list, 2)
+
+    return "単語bi-gram : {}\n文字bi-gram : {}".format(word_bi_gram, string_bi_gram)
+
+if __name__ == '__main__':
+    print(problem_no_05("I am an NLPer"))
---
+++ codes/capter_01/problem_no_06.py
@@ -0,0 +1,54 @@
+# coding = utf-8
+"""
+create on : 2017/08/06
+project name : NLP_100
+file name : problem_no_06 
+
+problem : "paraparaparadise"と"paragraph"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，
+          XとYの和集合，積集合，差集合を求めよ．さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
+"""
+
+from codes.capter_01.problem_no_05 import generate_n_gram
+
+
+def problem_no_06():
+    """ calculate bi-gram sets
+    :return: result message
+    """
+
+    x_string = "paraparaparadise"
+    y_string = "paragraph"
+
+    x = set(generate_n_gram(x_string, 2))
+    y = set(generate_n_gram(y_string, 2))
+
+    set_add = x | y
+    set_and = x & y
+
+    set_diff_x = x - y
+
+    set_diff_y = y - x
+
+    check_bi_gram = {"se"}
+
+    if x.issuperset(check_bi_gram):
+        se_contains_x = "'se'は、xに含まれます。"
+    else:
+        se_contains_x = "'se'は、xに含まれません。"
+
+    if y.issuperset(check_bi_gram):
+        se_contains_y = "'se'は、yに含まれます。"
+    else:
+        se_contains_y = "'se'は、yに含まれません。"
+
+    return_message = "xとyの和集合 : {}\n".format(set_add)
+    return_message += "xとyの積集合 : {}\n".format(set_and)
+    return_message += "xとyの差集合(x-y) : {}\n".format(set_diff_x)
+    return_message += "xとyの差集合(y-x) : {}\n\n".format(set_diff_y)
+    return_message += se_contains_x + "\n"
+    return_message += se_contains_y
+
+    return return_message
+
+if __name__ == '__main__':
+    print(problem_no_06())
