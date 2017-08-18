b5a35ad36335b3a9233266f888f431bf244e0eac
hyt <hytmachineworks@gmail.com>
2017-08-19 00:20:36 +0900

    solve problem no.14 to 15

---
+++ codes/chapter_02/problem_no_15.py
@@ -0,0 +1,56 @@
+# coding = utf-8
+"""
+create on : 2017/08/19
+project name : NLP_100
+file name : problem_no_15 
+
+This problem using hightemp.txt
+This file is available at "http://www.cl.ecei.tohoku.ac.jp/nlp100/".
+This file NOT include this repository.
+If you need file, please get above web site.
+
+problem : 自然数Nをコマンドライン引数などの手段で受け取り，
+          入力のうち末尾のN行だけを表示せよ．
+          確認にはtailコマンドを用いよ．
+
+make sure by bash command
+$ tail -n 5 hightemp.txt
+# ex. 5rows
+"""
+
+import csv
+import sys
+
+HIGHTEMP_TEXT_PATH = "./hightemp.txt"
+
+
+def problem_no_15():
+    """ Visible rows get by command line argument.
+        And output input rows to last row data.
+
+    :return: message string
+    """
+
+    with open(HIGHTEMP_TEXT_PATH, mode="r", encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file, delimiter="\t")
+        tsv_data = [row for row in reader]
+
+    args = sys.argv
+
+    if len(args) == 1:
+        return "Not found command line argument! Please input argument"
+
+    try:
+        output_rows = int(args[1])
+
+    except Exception as error_message:
+        print(error_message)
+        return "Invalid command line argument! Please retry correct numbers."
+
+    for row_data in tsv_data[-output_rows:]:
+        print("\t".join(row_data))
+
+    return "Output process complete"
+
+if __name__ == '__main__':
+    print(problem_no_15())
