4271a5d3b110356bfcae9ccc8d820daaf79aeb79
hyt <hytmachineworks@gmail.com>
2017-09-03 20:37:21 +0900

    add a comment of hightemp.txt

--- codes/chapter_02/problem_no_19.py
+++ codes/chapter_02/problem_no_19.py
@@ -2,7 +2,13 @@
 """
 create on : 2017/09/02
 project name : NLP_100
-file name : problem_no_19 
+file name : problem_no_19
+
+This problem using hightemp.txt
+This file is available at "http://www.cl.ecei.tohoku.ac.jp/nlp100/".
+This file NOT include this repository.
+If you need file, please get above web site.
+
 problem : 各行の1列目の文字列の出現頻度を求め，
           その高い順に並べて表示せよ．
           確認にはcut, uniq, sortコマンドを用いよ．
