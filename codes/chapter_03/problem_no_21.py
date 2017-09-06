40c056f81404c42fa1db65a182df30a902d16fd5
hyt <hytmachineworks@gmail.com>
2017-09-06 21:57:42 +0900

    Solve problem no.21

---
+++ codes/chapter_03/problem_no_21.py
@@ -0,0 +1,35 @@
+# coding = utf-8
+"""
+create on : 2017/09/06
+project name : NLP_100
+file name : problem_no_21 
+
+This problem using jawiki-country.json.gz
+This file is available at "http://www.cl.ecei.tohoku.ac.jp/nlp100/".
+This file NOT include this repository.
+If you need file, please get above web site.
+
+problem : 記事中でカテゴリ名を宣言している行を抽出せよ．
+
+"""
+
+from problem_no_20 import get_country_info
+import re
+from pprint import pprint
+
+
+def problem_no_21():
+    """ find a line definition of category name
+
+    :return: find lines list
+    """
+
+    country_info = get_country_info(listed=True)
+
+    category_def_line = [line_data for line_data in country_info
+                         if re.match(r"^==.*==$", line_data)]
+
+    return category_def_line
+
+if __name__ == '__main__':
+    pprint(problem_no_21())
