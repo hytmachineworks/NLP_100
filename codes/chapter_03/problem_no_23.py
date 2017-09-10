0b6aa1aaff13f94a335ec435d7b09e36705964ac
hyt <hytmachineworks@gmail.com>
2017-09-10 10:00:52 +0900

    solve problem no.23

--- codes/chapter_03/problem_no_23.py
+++ codes/chapter_03/problem_no_23.py
@@ -14,5 +14,50 @@ problem : 記事中に含まれるセクション名とそのレベル
 
 """
 
-if __name__ == '__main__':
-    pass
+from problem_no_20 import get_country_info
+import re
+from pprint import pprint
+
+
+def section_info(section_markup_text):
+    """ remove section markup
+        and return section name and level
+
+    :param section_markup_text:
+    :return: list
+    """
+
+    section_level = len(re.match(r"^==*", section_markup_text).group()) - 1
+    markup = "=" * (section_level + 1)
+    section_name = section_markup_text.replace(markup, "")
+
+    return [section_level, section_name]
+
+
+def get_section_name_and_level_list():
+    """ find a section line and get name and level
+        from a country info
+
+    :return: section info list
+    """
+
+    country_info = get_country_info(listed=True)
+
+    section_info_list = [section_info(line_data) for line_data in country_info
+                         if re.match(r"^==.*==$", line_data)]
+
+    return section_info_list
+
+
+def problem_no_23():
+    """ find a section line and get name and level
+        from a country info
+
+    :return: section info list
+    """
+
+    return get_section_name_and_level_list()
+
+
+if __name__ == "__main__":
+    pprint(problem_no_23())
