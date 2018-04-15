fad4b426f8a0173e0c73aed2cd2737e48ee5fb1f
hyt <hytmachineworks@gmail.com>
2018-04-15 20:25:14 +0900

    solve problem no.62

---
+++ codes/chapter_07/problem_no_62.py
@@ -0,0 +1,83 @@
+# coding = utf-8
+"""
+create on : 2018/03/11
+project name : NLP_100
+file name : problem_no_62
+
+This problem using nlp.txt
+This file is available at "http://www.cl.ecei.tohoku.ac.jp/nlp100/".
+This file NOT include this repository.
+If you need file, please get above web site.
+
+problem : 60で構築したデータベースを用い，
+          活動場所が「Japan」となっているアーティスト数を求めよ．
+"""
+
+import time
+
+from problem_no_60 import get_redis_connection
+from problem_no_61 import get_target_data
+
+
+def search_area_artists(area):
+    """ search artist from determine area
+
+    :param area: search area string
+    :return: search area artist list
+    """
+
+    def find_area_artist(start_cur, pool, r):
+        """ find area artist
+
+        :param start_cur: start cursor number
+        :param pool: connection pool
+        :param r: redis class
+        :return: area artist list
+        """
+
+        cur, keys = r.scan(start_cur, count=10000)
+
+        find_artists = [(key.decode(), get_target_data(key.decode(), pool, r))
+                        for key in keys
+                        if get_target_data(key.decode(), pool, r) == area]
+
+        return cur, find_artists
+
+    start = time.time()
+
+    area_artists = []
+
+    cursor = 0
+
+    flag = False
+
+    conn_pool, redis = get_redis_connection()
+
+    while not flag:
+
+        cursor, search_value = find_area_artist(cursor, conn_pool, redis)
+        area_artists += search_value
+
+        if cursor == 0:
+            flag = True
+
+    end = time.time()
+
+    print(end-start)
+
+    return area_artists
+
+
+def problem_no_62():
+    """ find all artist by determine area
+
+    :return: tuple list artist name and area
+    """
+
+    artist_name_area_list = search_area_artists(area="Japan")
+
+    print(artist_name_area_list)
+
+
+if __name__ == "__main__":
+    print(problem_no_62())
