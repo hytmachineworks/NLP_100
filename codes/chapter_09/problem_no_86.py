606b49257122e77a20e17ea76c6a9cc5f06f6eaf
hyt <hytmachineworks@gmail.com>
2018-12-16 17:25:30 +0900

    accept to pep8

--- codes/chapter_09/problem_no_86.py
+++ codes/chapter_09/problem_no_86.py
@@ -10,7 +10,8 @@ This file NOT include this repository.
 If you need file, please get above web site.
 
 problem : 85で得た単語の意味ベクトルを読み込み，"United States"のベクトルを表示せよ．
-          ただし，"United States"は内部的には"United_States"と表現されていることに注意せよ．
+          ただし，"United States"は内部的には"United_States"と
+          表現されていることに注意せよ．
 
 """
 import datetime
